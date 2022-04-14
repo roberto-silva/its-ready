cdcbf097a82a59e5c060151372eab27568542b69
roberto-silva <roberto.silva.oli@outlook.com>
2022-04-13 21:46:55 -0300

    crud_of_users - Improving user validation

--- back-end/TaProntoAPI/src/main/java/com/roberto/taPronto/dto/UserDTO.java
+++ back-end/TaProntoAPI/src/main/java/com/roberto/taPronto/dto/UserDTO.java
@@ -4,26 +4,40 @@ import com.roberto.taPronto.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
 	private Integer id;
+
 	@NotEmpty
+	@Size(min = 2, max = 100, message = "User name must be between 2 and 100 characters long.")
 	private String name;
+
 	@NotEmpty
+	@CPF(message = "Invalid cpf format.")
 	private String cpf;
+
 	@NotEmpty
 	private String role;
+
 	@NotEmpty
 	private String phone;
+
 	@NotEmpty
+	@Email(message = "Invalid email format.")
 	private String email;
+
 	@NotNull
+	@Valid
 	private AddressDTO address;
 
 	public UserDTO(User user){
@@ -35,6 +49,5 @@ public class UserDTO {
 		this.email = user.getEmail();
 		this.address = new AddressDTO(user.getAddress());
 	}
-
 
 }
