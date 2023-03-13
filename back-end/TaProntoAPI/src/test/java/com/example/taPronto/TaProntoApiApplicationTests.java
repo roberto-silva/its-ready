fda039d0af785695c6d3e4411e32739e31e05f4e
Roberto Silva de Oliveira Júnior <61337424+roberto-silva@users.noreply.github.com>
2023-03-12 23:10:28 -0300

    Ci cd (#4)
    * Improving initial configuration for CI/CD
    
    * Improving initial configuration for CI/CD
    
    * Improving initial configuration for CI/CD
    
    * Improving initial configuration for CI/CD
    
    * Improving initial configuration for CI/CD
    
    * Improving initial configuration for CI/CD

--- back-end/TaProntoAPI/src/test/java/com/example/taPronto/TaProntoApiApplicationTests.java
+++ back-end/TaProntoAPI/src/test/java/com/example/taPronto/TaProntoApiApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.taPronto;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@SpringBootConfiguration
 class TaProntoApiApplicationTests {
 
 	@Test
