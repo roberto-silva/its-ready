898103b1efd243c77eb6bf3c78728b0deb085e55
roberto-silva <roberto.silva.oli@outlook.com>
2023-03-25 22:39:30 -0300

    Improving initial configuration for CI/CD, add job to deploy the aplication

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/app/
-source environment/bin/activate
-supervisord -c supervisord.conf
+sudo apt-get install openjdk-11-jdk
+cd /home/ec2-user/app
+java -jar TaProntoAPI-0.0.1-SNAPSHOT.jar
