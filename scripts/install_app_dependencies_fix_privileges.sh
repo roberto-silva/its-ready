970eb028bf9db256350c7caaa1963bfd8f83099c
roberto-silva <roberto.silva.oli@outlook.com>
2023-03-26 17:41:43 -0300

    Improving initial configuration for CI/CD, add job to deploy the aplication

---
+++ scripts/install_app_dependencies_fix_privileges.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+chmod +x /home/ec2-user/server/* jar
+chmod +x /home/ec2-user/server/server_start.sh
+chmod +x /home/ec2-user/server/server_stop.sh
+sudo apt-get install openjdk-11-jdk
